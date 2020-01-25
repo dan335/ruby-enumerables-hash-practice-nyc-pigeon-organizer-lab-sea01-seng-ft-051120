5348edf4980cf0ce2367bfa76d10f5e04a6f7a67
Daniel Phillips <danphi@gmail.com>
2020-01-25 20:26:27 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,7 +3,20 @@ def nyc_pigeon_organizer(data)
   
   data.each do |category, attribute|
     attribute.each do |name|
-      if pigeons
+      
+      # create name if doesn't exist
+      if !(pigeons.key?(name))
+        pigeons[name] = {}
+      end
+      
+      # create category if it doesn't exist
+      if (!pigeons[name].key?(category))
+        pigeons[name][category] = []
+      end
+      
+      pigeons[name][category] << attribute
     end
   end
+  
+  pigeons
 end
